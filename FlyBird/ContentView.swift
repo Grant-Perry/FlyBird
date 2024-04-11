1f8a3a0dcd650786ab5a9812da8ec2a5cc60fa2f
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-04-11 09:44:11 -0400

    Initial Commit

---
+++ FlyBird/ContentView.swift
@@ -0,0 +1,26 @@
+//   ContentView.swift
+//   FlyBird
+//
+//   Created by: Grant Perry on 4/11/24 at 9:44 AM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlyBird/FlyBirdApp.swift
@@ -0,0 +1,19 @@
+//   FlyBirdApp.swift
+//   FlyBird
+//
+//   Created by: Grant Perry on 4/11/24 at 9:44 AM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+@main
+struct FlyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
